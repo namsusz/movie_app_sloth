079870dffdf0eb91cb6639b54c2958c93d902540
namsusz <61674318+namsusz@users.noreply.github.com>
2020-03-23 18:23:35 +0900

    Updates

---
+++ precache-manifest.636620f8eca73c69d88fa43a60cf6f82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d75be6d4b14b70ff5852fd28c617bd",
+    "url": "/movie_app_sloth/index.html"
+  },
+  {
+    "revision": "dca001d5d2f140cd748f",
+    "url": "/movie_app_sloth/static/css/main.8bef77df.chunk.css"
+  },
+  {
+    "revision": "3fb6bebdaf0a3fab3887",
+    "url": "/movie_app_sloth/static/js/2.8600ff04.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/movie_app_sloth/static/js/2.8600ff04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca001d5d2f140cd748f",
+    "url": "/movie_app_sloth/static/js/main.3dfc14df.chunk.js"
+  },
+  {
+    "revision": "786ecd12047ecf3feefd",
+    "url": "/movie_app_sloth/static/js/runtime-main.91791adc.js"
+  }
+]);
